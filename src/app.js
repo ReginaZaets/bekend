fc31084d399c620a3530ced1c16892cc69111a1b
ReginaZaets <regina.alibaeva21@gmail.com>
2024-07-15 13:26:27 +0300

    hw1 done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const http = require("http");
 const getUsers = require("./modules/users");
+const { URL } = require("url");
 
 const server = http.createServer((request, response) => {
+  const ipAdress = "http://127.0.0.1:3003";
+  const url = new URL(request.url, ipAdress);
+
+  const userName = url.searchParams.get("hello");
+  if (userName) {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "text/plain");
+    response.write(`Hello, ${userName}`);
+    response.end();
+    return;
+  }
+  if (request.url === "/?hello") {
+    response.statusCode = 400;
+    response.statusMessage = "Bad Request";
+    response.setHeader("Content-Type", "text/plain");
+    response.write("Enter a name");
+    response.end();
+    return;
+  }
+
   if (request.url === "/users") {
     response.statusCode = 200;
     response.statusMessage = "OK";
@@ -18,6 +40,12 @@ const server = http.createServer((request, response) => {
     response.end();
     return;
   }
+  response.statusCode = 500;
+  response.statusMessage = "Bad Request";
+  response.setHeader("Content-Type", "text/plain");
+  response.write("{}");
+  response.end();
+  return;
 });
 
 server.listen(3003, () => {
